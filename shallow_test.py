611de04b2d4fdfedaf72a06256eb276bb3e3dd48
MARTINEZ CHRISTOPHE p1709105 <christophe.martinez@etu.univ-lyon1.fr>
2023-10-06 11:54:58 +0200

    Shallow Test pour 1000 neurones

--- shallow_test.py
+++ shallow_test.py
@@ -2,7 +2,7 @@ from Projet import ShallowNetwork, grid_search_shallow
 import csv
 
 if __name__ == '__main__':
-    results = grid_search_shallow(ShallowNetwork, [10, 50, 100, 200, 500], [0.01, 0.001, 0.0001])
+    results = grid_search_shallow(ShallowNetwork, [10, 50, 100, 200, 500, 1000], [0.01, 0.001, 0.0001])
     f = open('./results_shallow.csv', 'w')
 
     # create the csv writer
